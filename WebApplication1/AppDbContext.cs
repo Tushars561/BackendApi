ecb0c505fa118865c247b40f03be982c5db71e90
Tushar <sonawanetushar561@gmail.com>
2025-05-27 12:50:45 +0530

    commit Api

---
+++ WebApplication1/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entity;
+
+namespace WebApplication1
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; } = null!;
+
+       
+    }
+}
